97e9ca47ec48dbdf15d17e1f395e4426b5ae3504
Pierre-Henry Soria <hi@ph7.me>
2019-02-03 18:31:27 +1300

    Swap App const to class structure + add state consts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>Robot App</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>
-  )
+const state = {
+  robots: robots,
+  searchField: ''
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="tc">
+        <h1>Robot App</h1>
+        <SearchBox />
+        <CardList robots={robots}/>
+      </div>
+    )
+  }
 }
 
 export default App;
