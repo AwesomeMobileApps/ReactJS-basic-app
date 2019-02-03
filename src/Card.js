477f800a45bfe4555cb6e278355b19843e11ea7d
Pierre-Henry Soria <hi@ph7.me>
2019-02-03 17:39:05 +1300

    Save the latest version of the app

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/CardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+  return (
+    <div>
+      {
+        robots.map((user, index) => {
+          return (
+            <Card
+              key={robots[index].id}
+              id={robots[index].id}
+              name={[robots[index].name]}
+              email={robots[index].email}
+            />
+          )
+        })
+      })
+    </div>
+  );
+}
+
+export default CardList;
