bb66110d8a7279e65f60accad87ad83885abe38e
Pierre-Henry Soria <hi@ph7.me>
2019-02-03 18:24:05 +1300

    Add SearchBox component to App + center text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import {robots} from './robots';
 
 const App = () => {
   return (
-    <CardList robots={robots}/>
+    <div className="tc">
+      <h1>Robot App</h1>
+      <SearchBox />
+      <CardList robots={robots}/>
+    </div>
   )
 }
 
